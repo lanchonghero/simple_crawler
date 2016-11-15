e17fdc768a3751496b8c532385fe27a95f0f61bb
LanChong <lanchonghero@163.com>
2016-11-15 22:53:21 +0800

    cmake modules

--- CMakeModules/FindPCRE.cmake
+++ CMakeModules/FindPCRE.cmake
@@ -1,4 +1,4 @@
-# - Find PCRE
+# Lan Chong<lanchonghero@163.com>
 # Find the PCRE includes and library
 #
 #  PCRE_INCLUDE_DIR - where to find pcre.h, etc.
@@ -6,25 +6,22 @@
 #  PCRE_FOUND       - True if PCRE found.
 
 IF (PCRE_INCLUDE_DIR)
-  # Already in cache, be silent
-  SET(PCRE_FIND_QUIETLY TRUE)
+  SET(PCRE_FIND_QUIETLY TRUE) # be silent if already in cache
 ENDIF (PCRE_INCLUDE_DIR)
-#从下面的路径找pcre.h，将找到的路径放到PCRE_INCLUDE_DIR这个变量中。
+
 FIND_PATH(PCRE_INCLUDE_DIR pcre.h
   /usr/local/include/pcre
   /usr/include/pcre
   /usr/include
 )
-#设置要寻找的链接库的名字，这里应该是找libpcre.so
+
 SET(PCRE_NAMES pcre)
-#将libpcre.so的路径放到PCRE_LIBRARY
 FIND_LIBRARY(PCRE_LIBRARY
   NAMES ${PCRE_NAMES}
   PATHS /usr/lib /usr/local/lib
   PATH_SUFFIXES pcre
 )
 
-#如果上面找到pcre的头文件和pcre的库文件，就将PCRE_FOUND设为真
 IF (PCRE_INCLUDE_DIR AND PCRE_LIBRARY)
   SET(PCRE_FOUND TRUE)
   SET( PCRE_LIBRARIES ${PCRE_LIBRARY} )
@@ -33,8 +30,6 @@ ELSE (PCRE_INCLUDE_DIR AND PCRE_LIBRARY)
   SET( PCRE_LIBRARIES )
 ENDIF (PCRE_INCLUDE_DIR AND PCRE_LIBRARY)
 
-#如果找到并且没有设置QUIET，则输出搜到动态库路径
-#如果找不到就输出错误信息
 IF (PCRE_FOUND)
   IF (NOT PCRE_FIND_QUIETLY)
     MESSAGE(STATUS "Found PCRE: ${PCRE_LIBRARY}")
@@ -46,7 +41,6 @@ ELSE (PCRE_FOUND)
   ENDIF (PCRE_FIND_REQUIRED)
 ENDIF (PCRE_FOUND)
 
-#将他标记为高级变量，具体什么作用没查到
 MARK_AS_ADVANCED(
   PCRE_LIBRARY
   PCRE_INCLUDE_DIR
